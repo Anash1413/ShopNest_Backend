b51d00682916082979ddd78b26c96aaba113290d
Maviz Ahmad <122000369+Anash1413@users.noreply.github.com>
2026-06-20 22:05:56 +0530

    Update CORS configuration to allow localhost:5174

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const cartRouter = require('./routes/cartRoutes')
 const favouritesRouter = require('./routes/favouritesRoutes')
 
  const app = express()
- app.use(cors({origin:[process.env.FRONTEND_URL,"http://localhost:5173/"],  credentials:true}))
+ app.use(cors({origin:[process.env.FRONTEND_URL,"http://localhost:5173","http://localhost:5174"],  credentials:true}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/api/auth" ,authRouter )
